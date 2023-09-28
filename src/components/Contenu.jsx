7d83c73337142b9bb5a874500f78fe90d9cf8e22
Akir Oussama <akir.oussama@gmail.com>
2023-09-28 08:57:57 +0200

    base app

---
+++ src/components/Contenu.jsx
@@ -0,0 +1,11 @@
+
+const contenu = () => {
+    return (
+        <div className="main-content">
+            <h2>Contenu de l'application</h2>
+            <p>Ceci est le contenu principal de l'application.</p>
+        </div>
+    );
+}
+
+export default contenu;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Sidebar() {
+
+    return (
+        <div className="sidebar">
+            <h3>Sidebar</h3>
+            <p>Item 1</p>
+            <p>Item 2</p>
+            <p>Item 3</p>
+        </div>
+
+    );
+}
+
+export default Sidebar;
